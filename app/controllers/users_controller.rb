4cfaf99a00c63d32188a8e3964150e3607259dfb
Preethikasd <mspreethika@gmail.com>
2017-08-09 00:45:25 +0800

    minor correction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,17 +3,16 @@ class UsersController < ApplicationController
 
 	def index
 		if user_signed_in?
-			@users = Follower.where(follow_id: current_user.id)
+			@users = Follower.where("follow_id=? AND count=?", current_user.id, 1)
 			@user_array = []
 			@users.each do |user|
 				@user_array << User.find(user.user_id)
 			end
 		end
 	end
-
 	def other_users
 			#get users i follow
-			@users_i_follow = Follower.where(follow_id: current_user.id)
+			@users_i_follow = Follower.where("follow_id=? AND count=?", current_user.id, 1)
 			#minus from total users to find users i dont follow
 			@users_i_follow_id = @users_i_follow.pluck(:user_id)
 			@users_i_dont_follow = User.all.pluck(:id) - @users_i_follow_id
